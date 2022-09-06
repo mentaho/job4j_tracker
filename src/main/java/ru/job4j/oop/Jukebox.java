53948a5403f3d884dee528561abf04035eb9fb7d
mentaho <bub.al@mail.ru>
2022-09-06 14:42:38 +0300

    1.3. Вызов метода с аргументами

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -3,27 +3,19 @@ package ru.job4j.oop;
 public class Jukebox {
 
     public void music(int position) {
+        String ls = System.lineSeparator();
         if (position == 1) {
-            System.out.println("Пусть бегут неуклюже"
-                               + System.lineSeparator()
-                               + "Пешеходы по лужам,"
-                               + System.lineSeparator()
-                               + "А вода по асфальту рекой."
-                               + System.lineSeparator()
-                               + "И неясно прохожим"
-                               + System.lineSeparator()
-                               + "В этот день непогожий,"
-                               + System.lineSeparator()
+            System.out.println("Пусть бегут неуклюже" + ls
+                               + "Пешеходы по лужам," + ls
+                               + "А вода по асфальту рекой." + ls
+                               + "И неясно прохожим" + ls
+                               + "В этот день непогожий," + ls
                                + "Почему я веселый такой.");
         } else if (position == 2) {
-            System.out.println("Спят усталые игрушки, книжки спят,"
-                               + System.lineSeparator()
-                               + "Одеяла и подушки ждут ребят."
-                               + System.lineSeparator()
-                               + "Даже сказка спать ложится,"
-                               + System.lineSeparator()
-                               + "Чтобы ночью нам присниться."
-                               + System.lineSeparator()
+            System.out.println("Спят усталые игрушки, книжки спят," + ls
+                               + "Одеяла и подушки ждут ребят." + ls
+                               + "Даже сказка спать ложится," + ls
+                               + "Чтобы ночью нам присниться." + ls
                                + "Ты ей пожелай: «Баю-бай!»");
 
         } else {
