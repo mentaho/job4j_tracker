39c2eac0374fd762d578ff74f1e696a01374c92b
mentaho <bub.al@mail.ru>
2022-09-13 09:24:55 +0300

    8. Что такое валидация?

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -40,7 +40,6 @@ public class Tracker {
         if (rsl) {
             item.setId(id);
             items[index] = item;
-            return rsl;
         }
         return rsl;
     }
@@ -53,7 +52,6 @@ public class Tracker {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            return rsl;
         }
         return rsl;
     }
