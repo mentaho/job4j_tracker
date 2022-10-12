93067f39e378a0d7b1d3b6e980b161e2755dcc4c
mentaho <bub.al@mail.ru>
2022-10-12 15:39:28 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,38 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+
+    }
+
+    public Tracker getInstance() {
+        return tracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
