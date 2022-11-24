c32af36cf05ece77202de180a991e7b4ae3c2b46
mentaho <bub.al@mail.ru>
2022-11-24 21:41:26 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String word : origin) {
+            check.add(word);
+        }
+        for (String s : text) {
+            if (!check.contains(s)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
