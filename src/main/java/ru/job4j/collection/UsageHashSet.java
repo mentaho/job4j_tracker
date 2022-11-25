919b868156d710f39a4fcee5624db3103c5a46da
mentaho <bub.al@mail.ru>
2022-11-25 09:38:38 +0300

    0. Set, HashSet, Iterator. [#10095 #364000]

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -9,10 +9,8 @@ public class UsageHashSet {
         autos.add("BMW");
         autos.add("Volvo");
         autos.add("Toyota");
-
         for (String auto : autos) {
             System.out.println(auto);
         }
-
     }
 }
