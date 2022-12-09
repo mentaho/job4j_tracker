99514342908196e34ec2d2d97cb090df4706226b
mentaho <bub.al@mail.ru>
2022-12-09 10:34:07 +0300

    3. Компаратор для строк [#35008]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,21 +5,11 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-
-        int length = Math.min(left.length(), right.length());
-        int result = 0;
-        for (int i = 0; i < length; i++) {
-            result = Character.compare(left.charAt(i), right.charAt(i));
-            if (result != 0) {
-                return result;
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return Character.compare(left.charAt(i), right.charAt(i));
             }
         }
-        if (left.length() > right.length()) {
-            return 1;
-        }
-        if (left.length() < right.length()) {
-            return -1;
-        }
-        return result;
+        return Integer.compare(left.length(), right.length());
     }
 }
