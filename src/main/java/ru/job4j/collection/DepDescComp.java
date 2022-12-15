801bdd9a04dccf7690305876c990823d5a547c01
mentaho <bub.al@mail.ru>
2022-12-15 10:35:35 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,21 +5,12 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String firstStringO1 = o1.contains("/") ? o1.substring(0, o1.indexOf("/")) : o1;
-        String secondStringO1 = o1.contains("/") ? o1.substring(o1.indexOf("/")) : "";
-        String firstStringO2 = o2.contains("/") ? o2.substring(0, o2.indexOf("/")) : o2;
-        String secondStringO2 = o2.contains("/") ? o2.substring(o2.indexOf("/")) : "";
-        Comparator<String> finalComparator = new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return firstStringO2.compareTo(firstStringO1);
-            }
-        }.thenComparing(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return secondStringO1.compareTo(secondStringO2);
-            }
-        });
-        return finalComparator.compare(o1, o2);
+        String[] array1 = o1.split("/");
+        String[] array2 = o2.split("/");
+        int rsl = array2[0].compareTo(array1[0]);
+        if (rsl == 0) {
+            return o1.compareTo(o2);
+        }
+        return rsl;
     }
 }
