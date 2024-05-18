5542a9307c43144239d29c922a1ca22b2586b4a2
mentaho <bub.al@mail.ru>
2024-05-18 12:22:34 -0500

    6. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -16,7 +16,7 @@ public class Broken {
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    void method(int a, int b, int c, int d, int e, int f) {
 
     }
 
