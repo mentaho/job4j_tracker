a7c7e9e81226d3266baeabddfc0d4bf7c9da2b4f
mentaho <bub.al@mail.ru>
2024-06-04 20:34:45 -0500

    6. Список адресов [#110062 #499183]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,11 +7,13 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress).collect(Collectors.toList());
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress())
+        return profiles.stream()
+                .map(Profile::getAddress)
                 .sorted(new Address.SortedByCity())
                 .distinct()
                 .toList();
